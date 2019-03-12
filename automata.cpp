24d70d22f47dd13fce492627fe7c14a0b657f23d
edward8 <ecormacoconnor@gmail.com>
2019-03-11 23:18:47 -0300

    1st try implementing game of life

--- automata.cpp
+++ automata.cpp
@@ -54,7 +54,7 @@ for(int i = 1; i<=rows; i++)
  }
 }
 
-int neighbor[rows][columns];
+int neighbors[rows][columns];
 //update the grid (i=row, q=column)
 //left unoccupied? ([i-1][q] == 0) (up:[i][q+1]) 2 = 0 3 = 1
 /* if(cell[i+1][q+1] == 0) then cell[i+1][q] = 3;*/
@@ -62,7 +62,45 @@ void updategrid ()
 {
 for(int i = 1; i<=rows-1; i++){
   for(int q = 1; q<=columns-1; q++){
-    
+    neighbors[i][q] = 0;
+//dudes above me
+    if(cell[i-1][q+1] == 1)
+      neighbors[i][q]++;
+    if(cell[i][q+1] == 1)
+      neighbors[i][q]++;
+    if(cell[i+1][q+1] == 1)
+      neighbors[i][q]++;
+//dudes to my left and right
+    if(cell[i-1][q] == 1)
+      neighbors[i][q]++;
+//  if(cell[i][q] == 1)
+//    neighbors[i][q]++;
+    if(cell[i+1][q] == 1)
+      neighbors[i][q]++;
+//dudes below me
+    if(cell[i-1][q-1] == 1)
+      neighbors[i][q]++;
+    if(cell[i][q-1] == 1)
+      neighbors[i][q]++;
+    if(cell[i+1][q-1] == 1)
+      neighbors[i][q]++;
+
+ if(cell[i][q] == 1)
+   {
+    if(neighbors[i][q] < 2)
+      cell[i][q] = 2;
+
+    if(neighbors[i][q] > 3)
+      cell[i][q] = 2;
+
+    if(neighbors[i][q] == 2 || neighbors[i][q] == 3)
+      cell[i][q] = 3;
+    }
+  if(cell[i][q] == 0)
+    {
+     if(neighbors[i][q] == 3)
+         cell[i][q] = 3;
+     }
   }
   //visualize updating
   //std:: cout << endl;
